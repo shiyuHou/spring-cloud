c916b4ad602c76cd1727e6e7ef35f5204d6e7c90
houshiyu <houshiyu@suiyueyule.com>
2019-11-21 18:33:13 +0800

    hystrix Turbine调试

--- expand/hystrix-turbine/src/main/java/com/spring/cloud/HystrixTurbineApplication.java
+++ expand/hystrix-turbine/src/main/java/com/spring/cloud/HystrixTurbineApplication.java
@@ -3,8 +3,10 @@ package com.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableHystrixDashboard
 @EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
